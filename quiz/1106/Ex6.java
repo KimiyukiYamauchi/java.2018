274277aa05dc55bd080a0236fd9e6c2c55a3f40a
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2018-11-03 06:41:03 +0900

    ex6_7, ex6_8, ex6_9仕様変更

--- quiz/1106/Ex6.java
+++ quiz/1106/Ex6.java
@@ -1,99 +1,105 @@
 class Ex6{
 
-	public int [] ex6_1(){
-		
-		int [] ret = new int[1];
-		
-		return ret;
-	}
+    public int [] ex6_1(){
 
-	public double [] ex6_2(){
-		
-		double [] ret = new double[1];
-		
-		return ret;
-	}
+        int [] ret = new int[1];
 
-	public int [] ex6_3(int a, int b){
-		
-		int [] ret = new int[1];
-		
-		return ret;
-	}
+        return ret;
+    }
 
-	public int [] ex6_4(int [] a){
-		
-		int [] ret = new int[1];
-		
-		return ret;
-	}
+    public double [] ex6_2(){
 
-	public int ex6_5(int [] a, int key){
-		
-		int ret = 9999;
-		
-		return ret;
-	}
+        double [] ret = new double[1];
 
-	public int ex6_6(int [] a, int key){
-		
-		int ret = 9999;
-		
-		return ret;
-	}
+        return ret;
+    }
 
-	public void ex6_7(int [] a, int idx){
-		
-		return;
-	}
+    public int [] ex6_3(int a, int b){
 
-	public void ex6_8(int [] a, int idx, int n){
-		
-		return;
-	}
+        int [] ret = new int[1];
 
-	public void ex6_9(int [] a, int idx, int x){
-		
-		return;
-	}
+        return ret;
+    }
 
-	public void ex6_10(int [] a, int [] b){
-		
-		return;
-	}
+    public int [] ex6_4(int [] a){
 
-	public int [] ex6_11(int [] a){
-		
-		int [] ret = new int[0];
+        int [] ret = new int[1];
 
-		return ret;
-	}
+        return ret;
+    }
 
-	public int [] ex6_12(int [] a, int x){
-		
-		int [] ret = new int[0];
+    public int ex6_5(int [] a, int key){
 
-		return ret;
-	}
+        int ret = 9999;
 
-	public int [] ex6_13(int [] a, int idx){
-		
-		int [] ret = new int[0];
+        return ret;
+    }
 
-		return ret;
-	}
-	
-	public int [] ex6_14(int [] a, int idx, int n){
-		
-		int [] ret = new int[0];
-		
-		return ret;
-	}
-	
-	public int [] ex6_15(int [] a, int idx, int x){
-		
-		int [] ret = new int[0];
-		
-		return ret;
-	}
+    public int ex6_6(int [] a, int key){
+
+        int ret = 9999;
+
+        return ret;
+    }
+
+    public int [] ex6_7(int [] a, int idx){
+
+        int [] ret = new int[1];
+
+        return ret;
+    }
+
+    public int [] ex6_8(int [] a, int idx, int n){
+
+        int [] ret = new int[1];
+
+        return ret;
+    }
+
+    public int [] ex6_9(int [] a, int idx, int x){
+
+        int [] ret = new int[1];
+
+        return ret;
+    }
+
+    public void ex6_10(int [] a, int [] b){
+
+        return;
+    }
+
+    public int [] ex6_11(int [] a){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_12(int [] a, int x){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_13(int [] a, int idx){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_14(int [] a, int idx, int n){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
+
+    public int [] ex6_15(int [] a, int idx, int x){
+
+        int [] ret = new int[0];
+
+        return ret;
+    }
 }
